c077fa0852815f1ccdff97c1763cdf7d7f05dab8
mykoduvayurofficial <30552669+mykoduvayurofficial@users.noreply.github.com>
2017-08-03 19:13:20 +0530

    Add files via upload

--- koduvayur.js
+++ koduvayur.js
@@ -290,30 +290,64 @@ mykdrApp.controller('feedbackController', function($rootScope, $scope) {
 });
 
 mykdrApp.directive('fbCommentBox', function() {
-    function createHTML(href, numposts, colorscheme, width) {
-        return '<div class="fb-comments" ' +
-            'data-href="' + href + '" ' +
-            'data-numposts="' + numposts + '" ' +
-            'data-colorsheme="' + colorscheme + '" ' +
-            'data-width="' + width + '">' +
-            '</div>';
-    }
 
-    return {
-        restrict: 'A',
-        scope: {},
-        link: function postLink(scope, elem, attrs) {
-            attrs.$observe('pageHref', function(newValue) {
-                var href = newValue;
-                var numposts = attrs.numposts || 5;
-                var colorscheme = attrs.colorscheme || 'light';
-                var width = attrs.width || '100%';
-                elem.html(createHTML(href, numposts, colorscheme, width));
-                FB.XFBML.parse(elem[0]);
-            });
-        }
-    };
+    // $scope.fbComments = $scope.fbComments = 'http://developers.facebook.com/docs/plugins/comments/';
+
 });
+
+
+// mykdrApp.directive('fbCommentBox', function() {
+//     function createHTML(href, numposts, colorscheme, width) {
+//         return '<div class="fb-comments" ' +
+//             'data-href="' + href + '" ' +
+//             'data-numposts="' + numposts + '" ' +
+//             'data-colorsheme="' + colorscheme + '" ' +
+//             'data-width="' + width + '">' +
+//             '</div>';
+//     }
+//
+//     return {
+//         restrict: 'A',
+//         scope: {},
+//         link: function postLink(scope, elem, attrs) {
+//             attrs.$observe('pageHref', function(newValue) {
+//                 var href = newValue;
+//                 var numposts = attrs.numposts || 20;
+//                 var colorscheme = attrs.colorscheme || 'light';
+//                 var width = attrs.width || '100%';
+//                 elem.html(createHTML(href, numposts, colorscheme, width));
+//                 FB.XFBML.parse(elem[0]);
+//             });
+//         }
+//     };
+// });
+
+
+
+    // function createHTML(href, numposts, colorscheme, width) {
+    //     return '<div class="fb-comments" ' +
+    //         'data-href="' + href + '" ' +
+    //         'data-numposts="' + numposts + '" ' +
+    //         'data-colorsheme="' + colorscheme + '" ' +
+    //         'data-width="' + width + '">' +
+    //         '</div>';
+    // }
+    //
+    // return {
+    //     restrict: 'A',
+    //     scope: {},
+    //     link: function postLink(scope, elem, attrs) {
+    //         attrs.$observe('pageHref', function(newValue) {
+    //             var href = newValue;
+    //             var numposts = attrs.numposts || 5;
+    //             var colorscheme = attrs.colorscheme || 'light';
+    //             var width = attrs.width || '100%';
+    //             elem.html(createHTML(href, numposts, colorscheme, width));
+    //             FB.XFBML.parse(elem[0]);
+    //         });
+    //     }
+    // };
+    //
 
 
 
